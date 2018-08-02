f4d4982af8cb2e950ff96825926968093958eb8c
Yurina Shin <yurinas@gmail.com>
2018-08-02 23:38:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,10 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-
-    resp.write "Morning"
+    time_now = Time.now.strftime('%H:%M')
+    time_compare_with = '12:00'
+    if time_now < tiem _compare_with
+    resp.write "Hello,World! Morning"
     resp.finish
   end
 
